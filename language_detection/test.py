6556ce8c74872f94c86d2dad8b25b0337c10daf2
TkrUdagawa <udagawa.takuro@lab.ntt.co.jp>
2013-04-10 11:54:36 +0900

    python example appended

---
+++ language_detection/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import json, commands
+from jubatus.classifier import client
+from jubatus.classifier import types
+
+while True:
+    buf = raw_input("> ")
+    classifier = client.classifier("127.0.0.1", 9199)
+    datum = types.datum([["text", buf.rstrip()]], [])
+    result = classifier.classify("", [datum])
+    if len(result[0]) == 0:
+        print("nothing")
+        continue
+    result[0].sort(key=lambda x:x.score, reverse=True)
+    for res in result[0]:
+        print(res.label + " -> " + str(res.score))
+
---
+++ language_detection/train.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys, json, commands, pprint
+import random
+from jubatus.classifier import client
+from jubatus.classifier import types
+
+NAME = "a"
+classifier = client.classifier("127.0.0.1", 9199)
+
+file_list=commands.getoutput("ls|grep _train.txt").split("\n")
+pp = pprint.PrettyPrinter()
+
+fds = map(lambda x: [x.replace("_train.txt", ""), open(x, "r")], file_list)
+while fds != "":
+    [label, fd] = random.choice(fds)
+    text = fd.readline()
+    if text == "":
+        fds.remove([label, fd])
+        print "finished train of label %s \n" % (label)
+        continue
+    text_strip = text.rstrip()
+    datum = types.datum([["text", text_strip]], [])
+    print "train %s : %s ..." %(label, text_strip)
+    classifier.train(NAME, [(label, datum)])
