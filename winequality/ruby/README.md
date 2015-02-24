9ac0abc9953a899cf0ab3cc174fe056896837759
KUMAZAKI Hiroki <kumazaki.hiroki@lab.ntt.co.jp>
2015-02-24 21:41:49 +0900

    add winequality example

---
+++ winequality/ruby/README.md
@@ -0,0 +1,21 @@
+# winequality
+
+First, boot up jubatus
+
+```
+$ jubaregression -f ../config.json
+```
+
+Next train it
+
+```
+$ ruby train.rb
+```
+
+And test it
+
+```
+$ ruby test.rb
+```
+
+enjoy!
---
+++ winequality/ruby/train.rb
@@ -0,0 +1,20 @@
+# -*- ruby coding:utf-8 -*-
+
+require 'jubatus/regression/client'
+require 'pp'
+
+NAME = "WINE"
+filename = "../winequality-white.csv"
+puts "train by #{filename}"
+
+cli = Jubatus::Regression::Client::Regression.new "127.0.0.1", 9199, NAME
+File.open(filename, "r") {|f|
+  names = f.readline.split(";").map{|n| n.gsub(/"/, "").chomp}
+  f.each_line {|line|
+    values = line.split(";").map{|n| n.to_f}
+    datum = Hash[*names.zip(values).flatten]
+    quality = datum['quality']
+    datum.delete('quality')
+    cli.train [[quality, Jubatus::Common::Datum.new(datum)]]
+  }
+}
