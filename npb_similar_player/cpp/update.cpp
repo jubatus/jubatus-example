13031b7a3d54ed2d234877821a99f6a05727c929
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2015-08-25 17:44:38 +0900

    eliminate pficommon in npb_similar_player C++ client (refs #17)

--- npb_similar_player/cpp/update.cpp
+++ npb_similar_player/cpp/update.cpp
@@ -3,14 +3,14 @@
 #include <sstream>
 #include <time.h>
 #include <sys/time.h>
+#include <jubatus/util/lang/cast.h>
 #include <jubatus/client/recommender_client.hpp>
 #include <jubatus/client/recommender_types.hpp>
-#include <pficommon/lang/util.h>
 
 using namespace std;
 using namespace jubatus;
 using namespace jubatus::recommender;
-using namespace pfi::lang;
+using namespace jubatus::util::lang;
 
 const string NAME = "recommender_ml";
 
@@ -37,24 +37,24 @@ int main(int argc, char* argv[]){
         >> obp >> ops >> rc27 >> xr27;
 
     d.add_string("チーム", team);
-    d.add_number("打率", pfi::lang::lexical_cast<float>(bave));
-    d.add_number("試合数", pfi::lang::lexical_cast<float>(games));
-    d.add_number("打席", pfi::lang::lexical_cast<float>(pa));
-    d.add_number("打数", pfi::lang::lexical_cast<float>(atbat));
-    d.add_number("安打", pfi::lang::lexical_cast<float>(hit));
-    d.add_number("本塁打", pfi::lang::lexical_cast<float>(homerun));
-    d.add_number("打点", pfi::lang::lexical_cast<float>(runsbat));
-    d.add_number("盗塁", pfi::lang::lexical_cast<float>(stolen));
-    d.add_number("四球", pfi::lang::lexical_cast<float>(bob));
-    d.add_number("死球", pfi::lang::lexical_cast<float>(hbp));
-    d.add_number("三振", pfi::lang::lexical_cast<float>(strikeout));
-    d.add_number("犠打", pfi::lang::lexical_cast<float>(sacrifice));
-    d.add_number("併殺打", pfi::lang::lexical_cast<float>(dp));
-    d.add_number("長打率", pfi::lang::lexical_cast<float>(slg));
-    d.add_number("出塁率", pfi::lang::lexical_cast<float>(obp));
-    d.add_number("OPS", pfi::lang::lexical_cast<float>(ops));
-    d.add_number("RC27", pfi::lang::lexical_cast<float>(rc27));
-    d.add_number("XR27", pfi::lang::lexical_cast<float>(xr27));
+    d.add_number("打率", lexical_cast<float>(bave));
+    d.add_number("試合数", lexical_cast<float>(games));
+    d.add_number("打席", lexical_cast<float>(pa));
+    d.add_number("打数", lexical_cast<float>(atbat));
+    d.add_number("安打", lexical_cast<float>(hit));
+    d.add_number("本塁打", lexical_cast<float>(homerun));
+    d.add_number("打点", lexical_cast<float>(runsbat));
+    d.add_number("盗塁", lexical_cast<float>(stolen));
+    d.add_number("四球", lexical_cast<float>(bob));
+    d.add_number("死球", lexical_cast<float>(hbp));
+    d.add_number("三振", lexical_cast<float>(strikeout));
+    d.add_number("犠打", lexical_cast<float>(sacrifice));
+    d.add_number("併殺打", lexical_cast<float>(dp));
+    d.add_number("長打率", lexical_cast<float>(slg));
+    d.add_number("出塁率", lexical_cast<float>(obp));
+    d.add_number("OPS", lexical_cast<float>(ops));
+    d.add_number("RC27", lexical_cast<float>(rc27));
+    d.add_number("XR27", lexical_cast<float>(xr27));
 
     r.update_row(pname, d);
   }
