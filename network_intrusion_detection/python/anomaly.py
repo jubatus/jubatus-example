afb13b2926a608217623ed5dbdb88f0091ef6bfa
Tetsuya SHIODA <ysk501sw@gmail.com>
2016-05-24 17:32:38 +0900

    fix comment from japanese to english

--- network_intrusion_detection/python/anomaly.py
+++ network_intrusion_detection/python/anomaly.py
@@ -15,13 +15,13 @@ def do_exit(sig, stack):
     sys.exit(0)
 
 if __name__ == '__main__':
-    # 0. KeyboardInterruptハンドラの設定
+    # 0. set KeyboardInterrupt handler
     signal.signal(signal.SIGINT, do_exit)
 
-    # 1.Jubatus Serverへの接続設定
+    # 1. set jubatus server
     anom = client.Anomaly("127.0.0.1", 9199, NAME)
 
-    # 2.学習用データの準備
+    # 2. prepare training data
     with open('../kddcup.data_10_percent.txt', mode='r') as file:
         for line in file:
             duration, protocol_type, service, flag, src_bytes, dst_bytes, land, wrong_fragment, urgent, hot, num_failed_logins, logged_in, num_compromised, root_shell, su_attempted, num_root, num_file_creations, num_shells, num_access_files, num_outbound_cmds, is_host_login, is_guest_login, count, srv_count, serror_rate, srv_serror_rate, rerror_rate, srv_rerror_rate, same_srv_rate, diff_srv_rate, srv_diff_host_rate, dst_host_count, dst_host_srv_count, dst_host_same_srv_rate, dst_host_diff_srv_rate, dst_host_same_src_port_rate, dst_host_srv_diff_host_rate, dst_host_serror_rate, dst_host_srv_serror_rate, dst_host_rerror_rate, dst_host_srv_rerror_rate, label = line[:-1].split(",")
@@ -76,9 +76,9 @@ if __name__ == '__main__':
                     ]:
                 datum.add_number(k, v)
 
-            # 3.データの学習（学習モデルの更新）
+            # 3. train data and update jubatus model
             ret = anom.add(datum)
 
-            # 4.結果の出力
+            # 4. output results
             if (ret.score != float('Inf')) and (ret.score!= 1.0):
                 print (ret, label)
